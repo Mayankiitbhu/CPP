e0948e3c4b6b553d4026279246e74fe21e984f5c
Mayankiitbhu <45464735+Mayankiitbhu@users.noreply.github.com>
2020-09-01 13:40:43 +0530

    Update Two_Flowers.cpp

--- Two_Flowers.cpp
+++ Two_Flowers.cpp
@@ -1,7 +1,11 @@
 /*
-Chef Go has planted a flower field — a grid with n rows and m columns. In each cell of this grid, a single flower is planted; let's denote the type of the flower planted in cell (i,j) by ai,j.
+Chef Go has planted a flower field — a grid with n rows and m columns. In each cell of this grid, a single flower is planted; let's denote the type of the flower
+planted in cell (i,j) by ai,j.
 
-Today, Chef Go is going to make a new menu with two dishes: Tom Yum Goong and Tom Kha Kai. He needs to pick as many flowers as possible to decorate the dishes and make them look attractive. Each dish can only be decorated with flowers of a single type, so he can only pick flowers of one type or two different types. Also, Chef Go wants the flowers he picks to form one connected area, so that he can finish his job quickly. A connected area is a set of cells such that we can move from any cell to any other cell in this set by only moving between side-adjacent cells from the set.
+Today, Chef Go is going to make a new menu with two dishes: Tom Yum Goong and Tom Kha Kai. He needs to pick as many flowers as possible to decorate the 
+dishes and make them look attractive. Each dish can only be decorated with flowers of a single type, so he can only pick flowers of one type or two
+different types. Also, Chef Go wants the flowers he picks to form one connected area, so that he can finish his job quickly. A connected
+area is a set of cells such that we can move from any cell to any other cell in this set by only moving between side-adjacent cells from the set.
 
 Help Chef Go find the size of the largest connected area in his flower field which contains at most two types of flowers
 */
