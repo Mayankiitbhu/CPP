69bb1bdc05bb3c0544a3a135f78331926fb743bf
Mayankiitbhu <45464735+Mayankiitbhu@users.noreply.github.com>
2020-10-06 13:43:36 +0530

    Create longest subarray with sum atmost k.cpp

---
+++ longest subarray with sum atmost k.cpp
@@ -0,0 +1,28 @@
+////negatives allowed////
+int longestSumArray(vector<int> v, int target){
+    
+    int n=v.size();
+    int s=0;
+    int ans=0;
+    vector<int> T(n);
+    
+    for(int i=0;i<n;i++){
+        s+=v[i];
+        if(s<=target)  ans=i+1;
+        else{
+           while(i-ans>0 && T[i-ans-1]>=s-target)  ans++;
+        }
+
+        if(i==0)T[i]=s;
+        else    T[i]=max(T[i-1],s);
+    }
+    return ans;
+}
+
+int main(void){
+    
+    cout<<longestSumArray({5,-10,7,-20,57},-22)<<endl;
+    
+    return 0;
+}
+// output 3 (the subarray={-10,7,-20})
